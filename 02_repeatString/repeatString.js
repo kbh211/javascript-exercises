597d98ed305fd239342616b4b7b8eb18c9a423a4
K Hals <kaylabhalsey@gmail.com>
2022-09-13 16:51:00 -0400

    02_repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, numRepeats) {
+    let repString = '';
+    if (numRepeats < 0){
+        return 'ERROR';
+    }
 
+    for (let i=0; i<numRepeats; i++){
+        repString += string;
+    }
+    return repString;
 };
 
 // Do not edit below this line
