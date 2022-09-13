c4876df46941c78eddb8edfaf27f23f1b704e0ff
K Hals <kaylabhalsey@gmail.com>
2022-09-13 19:15:44 -0400

    06_leapYears done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year%4 === 0){
+        console.log('yes');
+        if (year%100 === 0){
+            console.log('yes2');
+            return (year % 400 === 0 ? true : false);
+        }
+        return true;
+    }
+    return false;
 
 };
 
