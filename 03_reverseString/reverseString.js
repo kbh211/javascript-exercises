abe913423c89ca8d30c8ffbfb23a95890ad29711
K Hals <kaylabhalsey@gmail.com>
2022-09-13 17:04:54 -0400

    03_reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    if (string === '') return '';
+    for (let i=string.length-1; i>-1; i--){
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
