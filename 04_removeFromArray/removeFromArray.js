b8271f24766282bdf4a0d4740afac4d85faa0dae
K Hals <kaylabhalsey@gmail.com>
2022-09-13 17:48:51 -0400

    04_removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list) {
+    for (let i=1; i<arguments.length; i++){
+        for (let j=0; j<list.length; j++){
+            if (arguments[i] === list[j]){
+                list.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
